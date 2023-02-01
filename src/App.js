12ba0088a4fd65888d4586ac3615dc50f50e348f
David Luo <davidkluo@gmail.com>
2023-02-01 10:47:09 -0800

    Play with file chooser functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+import { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 function App() {
+  const [file, setFile] = useState();
+  const [isFileChosen, setIsFileChosen] = useState(false);
+
+  function handleChange(evt) {
+    setFile(evt.target.files[0]);
+    setIsFileChosen(true);
+    console.log('File chosen');
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+
+    const formData = new FormData();
+    formData.append('File', file);
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId='formFile'>
+          <Form.Label>Choose an image</Form.Label>
+          <Form.Control
+            name='imgFile'
+            type='file'
+            onChange={handleChange}
+          />
+        </Form.Group>
+
+        <Button variant='primary' type='submit'>Submit</Button>
+      </Form>
     </div>
   );
 }
