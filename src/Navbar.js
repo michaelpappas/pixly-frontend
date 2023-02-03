86f58e0e24066f98f65419ae7e45960f96bfd1fb
michaelpappas <pappas.michael@gmail.com>
2023-02-03 15:45:52 -0800

    add exifdata.css and navbar changes

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,16 +3,18 @@ import Navbar from 'react-bootstrap/Nav';
 import { NavLink } from 'react-router-dom';
 import { Images, PlusCircleDotted } from 'react-bootstrap-icons';
 import './NavBar.css';
+import { NavbarBrand } from 'react-bootstrap';
 
 function NavBar() {
   return (
     <Navbar className='NavBar shadow' bg='dark' variant='dark'>
+      <NavbarBrand href="/" className='ms-3 mt-1'>Pixly</NavbarBrand>
       <Nav className='mx-auto'>
-        <NavLink className='nav-link' to='/'><Images/></NavLink>
-        <NavLink className='nav-link' to='/new'><PlusCircleDotted/></NavLink>
+        <NavLink className='nav-link' to='/'><Images /></NavLink>
+        <NavLink className='nav-link' to='/new'><PlusCircleDotted /></NavLink>
       </Nav>
     </Navbar>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
